e1f3f095a2847442c2b85d2885a1fd066540e6fa
Michael Rodgers <mail@mjrodge.me>
2018-03-15 16:34:26 -0400

    Poll Schema Created
    Created two schema, one for answers, the other for polls

---
+++ app/models/poll.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AnswerSchema = new Schema({
+  answer: String,
+  votes: Number
+});
+
+var PollSchema = new Schema({
+  question: String,
+  answers: [AnswerSchema]
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
