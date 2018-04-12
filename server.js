b8d8bb2586193bb5093c2e6a71fb2dd11cd1f71f
Michael Rodgers <mail@mjrodge.me>
2018-04-12 13:52:12 -0400

    Methods Update
    Added header allowing additional PUT and DELETE methods

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ router.use(function(req, res, next) {
     // do logging
     res.header("Access-Control-Allow-Origin", "*"); // Added to resolve CORS issue
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
