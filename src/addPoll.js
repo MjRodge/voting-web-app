99099ec3aca7ade3ecdc2f0c2a157e1f0cca0069
Michael Rodgers <mail@mjrodge.me>
2018-04-12 14:44:30 -0400

    Add Poll Method
    Added a button that would allow users to add a poll

---
+++ src/addPoll.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import Modal from 'react-responsive-modal';
+
+class AddPollModal extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        open: false,
+        poll: '',
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.apiLink = "http://127.0.0.1:8080/api/polls/";
+  }
+
+  onOpenModal = () => {
+    this.setState({open: true});
+  }
+  onCloseModal = () => {
+    this.setState({open: false});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({poll : this.element.value });
+    axios.post(this.apiLink, {question: this.element.value})
+      .then(function(response){
+        console.log('saved successfully')
+      });
+  }
+
+  render() {
+    const {open} = this.state;
+    return (
+      <div className="add-poll">
+        <button className="btn btn-action" onClick={this.onOpenModal}>
+          Add Poll
+        </button>
+        <Modal open={open} onClose={this.onCloseModal} little>
+          <h2>Add A Poll</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="Poll">
+              Poll:
+              <input type="text" name="poll" placeholder="Question" ref={el => this.element = el} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default AddPollModal;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Polls from "./polls"
+import Polls from './polls';
+import AddPollModal from './addPoll';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <AddPollModal />
         <Polls />
       </div>
     );
