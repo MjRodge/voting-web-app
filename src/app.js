cc5201828aea82b7e77c17670ec6c3f5abfaf06e
Michael Rodgers <mail@mjrodge.me>
2018-04-25 12:09:01 -0400

    MaterialUI
    Installed MaterialUI module

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Polls from './polls';
 import PollResult from './pollResult';
 import Header from './header';
@@ -7,13 +8,15 @@ import Header from './header';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route path="/" component={Header} />
-          <Route exact path="/polls" component={Polls} />
-          <Route path="/polls/:pollId" component={PollResult} />
-        </div>
-      </Router>
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <Route path="/" component={Header} />
+            <Route exact path="/polls" component={Polls} />
+            <Route path="/polls/:pollId" component={PollResult} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
